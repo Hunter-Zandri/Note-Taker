bab02891cb15b9f09b680e033be6aa482374ca0e
Hunter-Zandri <86785668+Hunter-Zandri@users.noreply.github.com>
2021-12-02 12:28:44 -0500

    files should be ready to deploy using heroku

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-
 const express = require('express');
 
-
-
 const app = express();
-const PORT = 3001;
 
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-app.use(express.statick('public'));
 
 require('./routes/apiroutes')(app);
 require('./routes/htmlroutes')(app);
-
 
 app.listen(PORT, function() {
 console.log(`Server is listening on PORT: ${PORT}`);
