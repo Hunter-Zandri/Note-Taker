9bcf6cf6985b7d4cbb0ad2384439055eba65e3ad
Hunter-Zandri <zandrihunter@gmail.com>
2022-02-28 16:25:08 -0500

    changing file structure

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
 
 app.listen(PORT, function() {
 console.log(`Server is listening on PORT: ${PORT}`);
